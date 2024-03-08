69fcc7e386751beaef2066cd0b91180709753dff
Peter S. Bornerup <pb@onomondo.com>
2024-03-08 11:14:29 +0100

    feat: add optional external heap allocator
    Add an optional heap allocator.
    Add heap information during compilation.
    Make sure to use default heap for autoconf build
    and use system heap for test build.

--- include/onomondo/softsim/mem.h
+++ include/onomondo/softsim/mem.h
@@ -6,8 +6,16 @@
 
 #pragma once
 
+#ifdef CONFIG_USE_SYSTEM_HEAP
 #include <stdlib.h>
 
 #define SS_ALLOC(obj) malloc(sizeof(obj))
 #define SS_ALLOC_N(n) malloc(n)
 #define SS_FREE(obj) free(obj)
+#else  // DEFAULT
+void *port_malloc(size_t);
+void port_free(void *);
+#define SS_ALLOC(obj) port_malloc(sizeof(obj));
+#define SS_ALLOC_N(n) port_malloc(n);
+#define SS_FREE(obj) port_free(obj);
+#endif	// CONFIG_USE_SYSTEM_HEAP
--- src/softsim/crypto/common.h
+++ src/softsim/crypto/common.h
@@ -9,18 +9,18 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <onomondo/softsim/mem.h>
 #define MSG_DEBUG
 #define wpa_hexdump(x, args...)
 #define wpa_hexdump_key(x, args...)
 #define wpa_printf(x, args...)
 
-#define os_memcpy(x, y, z)  memcpy(x, y, z)
-#define os_memcmp(x, y, z)  memcmp(x, y, z)
-#define os_memcmp_const(x, y, z)  memcmp(x, y, z)
-#define os_memset(x, y, z)  memset(x, y, z)
-#define os_malloc(x)  malloc(x)
-#define os_free(x)  free(x)
+#define os_memcpy(x, y, z) memcpy(x, y, z)
+#define os_memcmp(x, y, z) memcmp(x, y, z)
+#define os_memcmp_const(x, y, z) memcmp(x, y, z)
+#define os_memset(x, y, z) memset(x, y, z)
+#define os_malloc(x) SS_ALLOC_N(x)
+#define os_free(x) SS_FREE(x)
 
 typedef uint8_t u8;
 typedef uint32_t u32;
