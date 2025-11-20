055a30362750e1b2f7b95bf73d000453c456c34c
Benjamin Bruun <bb@onomondo.com>
2025-11-20 18:16:19 +0000

    feat: introduce provision util

---
+++ include/onomondo/utils/ss_provision.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024 Onomondo ApS. All rights reserved.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#include <stdint.h>
+
+int onomondo_profile_provisioning(const char *profile);
