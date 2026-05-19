382d09f86ba5d6c755158f4618138a77dd281bbf
Farhad H. P. Shirvan <fs@onomondo.com>
2026-05-19 13:48:09 +0200

    Add robust concatenated SMS-PP Data Download support
    Signed-off-by: Farhad H. P. Shirvan <fs@onomondo.com>

--- tests/sms/sms_test.c
+++ tests/sms/sms_test.c
@@ -9,9 +9,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
 #include <onomondo/softsim/utils.h>
 #include <onomondo/softsim/list.h>
+#include "src/softsim/uicc/sw.h"
 #include "src/softsim/uicc/sms.h"
+#include "src/softsim/uicc/uicc_sms_rx.h"
 #include "src/softsim/uicc/uicc_sms_tx.h"
 #include "src/softsim/uicc/context.h"
 
@@ -19,6 +22,7 @@
 static void ready_ctx(struct ss_context *ctx)
 {
 	memset(ctx, 0, sizeof(*ctx));
+	ss_uicc_sms_rx_clear(ctx);
 	ss_uicc_sms_tx_clear(ctx);
 	/* Simulate context that set all the relevant bits */
 	ctx->proactive.term_profile[0] = 0xff;
@@ -302,6 +306,264 @@ void ss_uicc_sms_tx_test_multi(void)
 	ss_uicc_sms_tx_clear(&ctx);
 }
 
+static size_t make_sms_deliver_with_udhl(uint8_t *sms_tpdu, size_t sms_tpdu_len, const uint8_t tp_oa[4],
+					 uint8_t udhl, const uint8_t *ud_hdr, size_t ud_hdr_len,
+					 const uint8_t *tp_ud, size_t tp_ud_len)
+{
+	static const uint8_t scts[7] = { 0x00, 0x11, 0x29, 0x12, 0x00, 0x00, 0x04 };
+	size_t pos = 0;
+	size_t tp_udl = 1 + ud_hdr_len + tp_ud_len;
+
+	assert(tp_udl <= 255);
+	assert(sms_tpdu_len >= 18 + ud_hdr_len + tp_ud_len);
+
+	sms_tpdu[pos++] = 0x40; /* SMS-DELIVER with TP-UDHI */
+	sms_tpdu[pos++] = 0x08;
+	sms_tpdu[pos++] = 0x81;
+	memcpy(&sms_tpdu[pos], tp_oa, 4);
+	pos += 4;
+	sms_tpdu[pos++] = 0x7f;
+	sms_tpdu[pos++] = 0xf6;
+	memcpy(&sms_tpdu[pos], scts, sizeof(scts));
+	pos += sizeof(scts);
+	sms_tpdu[pos++] = (uint8_t)tp_udl;
+	sms_tpdu[pos++] = udhl;
+	if (ud_hdr_len > 0) {
+		memcpy(&sms_tpdu[pos], ud_hdr, ud_hdr_len);
+		pos += ud_hdr_len;
+	}
+	if (tp_ud_len > 0) {
+		memcpy(&sms_tpdu[pos], tp_ud, tp_ud_len);
+		pos += tp_ud_len;
+	}
+
+	return pos;
+}
+
+static size_t make_sms_deliver(uint8_t *sms_tpdu, size_t sms_tpdu_len, const uint8_t tp_oa[4],
+			       const uint8_t *ud_hdr, size_t ud_hdr_len, const uint8_t *tp_ud, size_t tp_ud_len)
+{
+	assert(ud_hdr_len <= 255);
+	return make_sms_deliver_with_udhl(sms_tpdu, sms_tpdu_len, tp_oa, (uint8_t)ud_hdr_len, ud_hdr,
+					  ud_hdr_len, tp_ud, tp_ud_len);
+}
+
+static int sms_rx_part(struct ss_context *ctx, const uint8_t tp_oa[4], const uint8_t *ud_hdr, size_t ud_hdr_len,
+		       const uint8_t *tp_ud, size_t tp_ud_len, size_t *response_len)
+{
+	uint8_t sms_tpdu[256];
+	uint8_t response[256];
+	struct ss_buf buf;
+	size_t sms_tpdu_len;
+
+	sms_tpdu_len = make_sms_deliver(sms_tpdu, sizeof(sms_tpdu), tp_oa, ud_hdr, ud_hdr_len, tp_ud, tp_ud_len);
+	buf.data = sms_tpdu;
+	buf.len = sms_tpdu_len;
+	*response_len = sizeof(response);
+	return ss_uicc_sms_rx(ctx, &buf, response_len, response);
+}
+
+static void expect_sms_rx_part(struct ss_context *ctx, const uint8_t tp_oa[4], const uint8_t *ud_hdr,
+			       size_t ud_hdr_len, const uint8_t *tp_ud, size_t tp_ud_len, int expected_rc,
+			       size_t expected_response_len)
+{
+	size_t response_len;
+	int rc;
+
+	rc = sms_rx_part(ctx, tp_oa, ud_hdr, ud_hdr_len, tp_ud, tp_ud_len, &response_len);
+	assert(rc == expected_rc);
+	assert(response_len == expected_response_len);
+}
+
+static void ss_uicc_sms_rx_concat_8bit_test(void)
+{
+	static const uint8_t tp_oa[4] = { 0x55, 0x66, 0x77, 0x88 };
+	struct ss_context ctx;
+	uint8_t udh_1[] = { 0x00, 0x03, 0x21, 0x02, 0x01 };
+	uint8_t udh_2[] = { 0x00, 0x03, 0x21, 0x02, 0x02 };
+	uint8_t tp_ud_1[] = { 0xde, 0xad };
+	uint8_t tp_ud_2[] = { 0xbe, 0xef };
+
+	printf("test ss_uicc_sms_rx concat 8-bit\n");
+	ready_ctx(&ctx);
+	expect_sms_rx_part(&ctx, tp_oa, udh_1, sizeof(udh_1), tp_ud_1, sizeof(tp_ud_1), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 1);
+	expect_sms_rx_part(&ctx, tp_oa, udh_2, sizeof(udh_2), tp_ud_2, sizeof(tp_ud_2), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
+static void ss_uicc_sms_rx_concat_16bit_test(void)
+{
+	static const uint8_t tp_oa[4] = { 0x55, 0x66, 0x77, 0x88 };
+	struct ss_context ctx;
+	uint8_t udh_1[] = { 0x08, 0x04, 0x12, 0x34, 0x02, 0x01 };
+	uint8_t udh_2[] = { 0x08, 0x04, 0x12, 0x34, 0x02, 0x02 };
+	uint8_t tp_ud_1[] = { 0x10 };
+	uint8_t tp_ud_2[] = { 0x20 };
+
+	printf("test ss_uicc_sms_rx concat 16-bit\n");
+	ready_ctx(&ctx);
+	expect_sms_rx_part(&ctx, tp_oa, udh_1, sizeof(udh_1), tp_ud_1, sizeof(tp_ud_1), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 1);
+	expect_sms_rx_part(&ctx, tp_oa, udh_2, sizeof(udh_2), tp_ud_2, sizeof(tp_ud_2), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
+static void ss_uicc_sms_rx_concat_out_of_order_test(void)
+{
+	static const uint8_t tp_oa[4] = { 0x55, 0x66, 0x77, 0x88 };
+	struct ss_context ctx;
+	uint8_t udh_1[] = { 0x00, 0x03, 0x22, 0x02, 0x01 };
+	uint8_t udh_2[] = { 0x00, 0x03, 0x22, 0x02, 0x02 };
+	uint8_t tp_ud_1[] = { 0x01 };
+	uint8_t tp_ud_2[] = { 0x02 };
+
+	printf("test ss_uicc_sms_rx concat out-of-order\n");
+	ready_ctx(&ctx);
+	expect_sms_rx_part(&ctx, tp_oa, udh_2, sizeof(udh_2), tp_ud_2, sizeof(tp_ud_2), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 1);
+	expect_sms_rx_part(&ctx, tp_oa, udh_1, sizeof(udh_1), tp_ud_1, sizeof(tp_ud_1), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
+static void ss_uicc_sms_rx_concat_duplicate_test(void)
+{
+	static const uint8_t tp_oa[4] = { 0x55, 0x66, 0x77, 0x88 };
+	struct ss_context ctx;
+	uint8_t udh_1[] = { 0x00, 0x03, 0x23, 0x02, 0x01 };
+	uint8_t udh_2[] = { 0x00, 0x03, 0x23, 0x02, 0x02 };
+	uint8_t tp_ud_1[] = { 0x01 };
+	uint8_t tp_ud_2[] = { 0x02 };
+
+	printf("test ss_uicc_sms_rx concat duplicate\n");
+	ready_ctx(&ctx);
+	expect_sms_rx_part(&ctx, tp_oa, udh_1, sizeof(udh_1), tp_ud_1, sizeof(tp_ud_1), 0, 0);
+	expect_sms_rx_part(&ctx, tp_oa, udh_1, sizeof(udh_1), tp_ud_1, sizeof(tp_ud_1), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 1);
+	expect_sms_rx_part(&ctx, tp_oa, udh_2, sizeof(udh_2), tp_ud_2, sizeof(tp_ud_2), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
+static void ss_uicc_sms_rx_concat_conflicting_duplicate_test(void)
+{
+	static const uint8_t tp_oa[4] = { 0x55, 0x66, 0x77, 0x88 };
+	struct ss_context ctx;
+	uint8_t udh_1[] = { 0x00, 0x03, 0x24, 0x02, 0x01 };
+	uint8_t tp_ud_1[] = { 0x01 };
+	uint8_t tp_ud_1_conflict[] = { 0x99 };
+
+	printf("test ss_uicc_sms_rx concat conflicting duplicate\n");
+	ready_ctx(&ctx);
+	expect_sms_rx_part(&ctx, tp_oa, udh_1, sizeof(udh_1), tp_ud_1, sizeof(tp_ud_1), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 1);
+	expect_sms_rx_part(&ctx, tp_oa, udh_1, sizeof(udh_1), tp_ud_1_conflict, sizeof(tp_ud_1_conflict),
+			   SS_SW_ERR_WRONG_PARAM_INCORRECT_DATA, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
+static void ss_uicc_sms_rx_concat_interleaved_test(void)
+{
+	static const uint8_t tp_oa[4] = { 0x55, 0x66, 0x77, 0x88 };
+	struct ss_context ctx;
+	uint8_t udh_a1[] = { 0x00, 0x03, 0x31, 0x02, 0x01 };
+	uint8_t udh_a2[] = { 0x00, 0x03, 0x31, 0x02, 0x02 };
+	uint8_t udh_b1[] = { 0x00, 0x03, 0x32, 0x02, 0x01 };
+	uint8_t udh_b2[] = { 0x00, 0x03, 0x32, 0x02, 0x02 };
+	uint8_t tp_ud[] = { 0x01 };
+
+	printf("test ss_uicc_sms_rx concat interleaved refs\n");
+	ready_ctx(&ctx);
+	expect_sms_rx_part(&ctx, tp_oa, udh_a1, sizeof(udh_a1), tp_ud, sizeof(tp_ud), 0, 0);
+	expect_sms_rx_part(&ctx, tp_oa, udh_b1, sizeof(udh_b1), tp_ud, sizeof(tp_ud), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 2);
+	expect_sms_rx_part(&ctx, tp_oa, udh_a2, sizeof(udh_a2), tp_ud, sizeof(tp_ud), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 1);
+	expect_sms_rx_part(&ctx, tp_oa, udh_b2, sizeof(udh_b2), tp_ud, sizeof(tp_ud), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
+static void ss_uicc_sms_rx_concat_same_ref_different_originator_test(void)
+{
+	static const uint8_t tp_oa_a[4] = { 0x55, 0x66, 0x77, 0x88 };
+	static const uint8_t tp_oa_b[4] = { 0x55, 0x66, 0x77, 0x98 };
+	struct ss_context ctx;
+	uint8_t udh_1[] = { 0x00, 0x03, 0x33, 0x02, 0x01 };
+	uint8_t udh_2[] = { 0x00, 0x03, 0x33, 0x02, 0x02 };
+	uint8_t tp_ud[] = { 0x01 };
+
+	printf("test ss_uicc_sms_rx concat same ref different originator\n");
+	ready_ctx(&ctx);
+	expect_sms_rx_part(&ctx, tp_oa_a, udh_1, sizeof(udh_1), tp_ud, sizeof(tp_ud), 0, 0);
+	expect_sms_rx_part(&ctx, tp_oa_b, udh_1, sizeof(udh_1), tp_ud, sizeof(tp_ud), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 2);
+	expect_sms_rx_part(&ctx, tp_oa_a, udh_2, sizeof(udh_2), tp_ud, sizeof(tp_ud), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 1);
+	expect_sms_rx_part(&ctx, tp_oa_b, udh_2, sizeof(udh_2), tp_ud, sizeof(tp_ud), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
+static void ss_uicc_sms_rx_concat_cpi_first_part_only_test(void)
+{
+	static const uint8_t tp_oa[4] = { 0x55, 0x66, 0x77, 0x88 };
+	struct ss_context ctx;
+	uint8_t udh_1[] = { 0x00, 0x03, 0x34, 0x02, 0x01, 0x70, 0x01, 0xaa };
+	uint8_t udh_2[] = { 0x00, 0x03, 0x34, 0x02, 0x02 };
+	uint8_t tp_ud_1[] = { 0xde };
+	uint8_t tp_ud_2[] = { 0xad };
+
+	printf("test ss_uicc_sms_rx concat CPI on first part only\n");
+	ready_ctx(&ctx);
+	expect_sms_rx_part(&ctx, tp_oa, udh_1, sizeof(udh_1), tp_ud_1, sizeof(tp_ud_1), 0, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 1);
+	expect_sms_rx_part(&ctx, tp_oa, udh_2, sizeof(udh_2), tp_ud_2, sizeof(tp_ud_2),
+			   SS_SW_ERR_CHECKING_WRONG_LENGTH, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
+static void ss_uicc_sms_rx_concat_malformed_test(void)
+{
+	static const uint8_t tp_oa[4] = { 0x55, 0x66, 0x77, 0x88 };
+	struct ss_context ctx;
+	uint8_t sms_tpdu[256];
+	uint8_t response[256];
+	struct ss_buf buf;
+	size_t sms_tpdu_len;
+	size_t response_len;
+	int rc;
+	uint8_t malformed_concat[] = { 0x00, 0x02, 0x41, 0x02 };
+	uint8_t invalid_seq[] = { 0x00, 0x03, 0x41, 0x02, 0x00 };
+	uint8_t tp_ud[] = { 0x01 };
+
+	printf("test ss_uicc_sms_rx concat malformed\n");
+	ready_ctx(&ctx);
+
+	sms_tpdu_len = make_sms_deliver_with_udhl(sms_tpdu, sizeof(sms_tpdu), tp_oa, 5, NULL, 0, NULL, 0);
+	buf.data = sms_tpdu;
+	buf.len = sms_tpdu_len;
+	response_len = sizeof(response);
+	rc = ss_uicc_sms_rx(&ctx, &buf, &response_len, response);
+	assert(rc == SS_SW_ERR_WRONG_PARAM_INCORRECT_DATA);
+	assert(response_len == 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+
+	expect_sms_rx_part(&ctx, tp_oa, malformed_concat, sizeof(malformed_concat), tp_ud, sizeof(tp_ud),
+			   SS_SW_ERR_WRONG_PARAM_INCORRECT_DATA, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+
+	expect_sms_rx_part(&ctx, tp_oa, invalid_seq, sizeof(invalid_seq), tp_ud, sizeof(tp_ud),
+			   SS_SW_ERR_WRONG_PARAM_INCORRECT_DATA, 0);
+	assert(ctx.proactive.sms_rx_state.reassembly_count == 0);
+	ss_uicc_sms_rx_clear(&ctx);
+}
+
 int main(int argc, char **argv)
 {
 	ss_sms_hdr_decode_sms_deliver_test();
@@ -312,5 +574,14 @@ int main(int argc, char **argv)
 	ss_sms_hdr_encode_test_sms_submit();
 	ss_uicc_sms_tx_test_single();
 	ss_uicc_sms_tx_test_multi();
+	ss_uicc_sms_rx_concat_8bit_test();
+	ss_uicc_sms_rx_concat_16bit_test();
+	ss_uicc_sms_rx_concat_out_of_order_test();
+	ss_uicc_sms_rx_concat_duplicate_test();
+	ss_uicc_sms_rx_concat_conflicting_duplicate_test();
+	ss_uicc_sms_rx_concat_interleaved_test();
+	ss_uicc_sms_rx_concat_same_ref_different_originator_test();
+	ss_uicc_sms_rx_concat_cpi_first_part_only_test();
+	ss_uicc_sms_rx_concat_malformed_test();
 	return 0;
 }
